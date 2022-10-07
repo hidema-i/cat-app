909994be91ea19519f6d1fcf229a8a04f13abd64
ShuumaIwamoto <iwa06ki12da@gmail.com>
2022-10-07 16:04:12 +0900

    catApp-test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,36 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import type { GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
+import "semantic-ui-css/semantic.min.css";
+import { Loader } from "semantic-ui-react";
+interface SearchCatimage {
+  id: string;
+  url: string;
+  width: number;
+  height: number;
+}
 
-const Home: NextPage = () => {
-  const fetchCatImage = async () => {
-    const res = await fetch("https://api.thecatapi.com/v1/images/search");
-    const result = await res.json();
-    return result[0];
-  };
+interface IndexPageProps {
+  initialCatimageurl: string;
+}
+
+const fetchCatImage = async (): Promise<SearchCatimage> => {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const result = await res.json();
+  return result[0];
+};
+
+const Home: NextPage<IndexPageProps> = ({ initialCatimageurl }) => {
+  const [catImageUrl, setCatimageUrl] = useState(initialCatimageurl);
+  //Loading
+  const [isloading, setIsLoading] = useState(false);
 
   const handleClick = async () => {
+    //handleClickをクリックした時に（true）にする
+    setIsLoading(true);
     const catImage = await fetchCatImage();
-    console.log(catImage);
+    setCatimageUrl(catImage.url);
+    //handleClickで読み込めたらを（false）にする
+    setIsLoading(false);
   };
 
   return (
@@ -26,16 +44,27 @@ const Home: NextPage = () => {
       }}
     >
       <h1>catImageApp</h1>
-      <img
-        src="https://cdn2.thecatapi.com/images/e9e.jpg"
-        width={500}
-        height="auto"
-      />
-      <button style={{ marginTop: "18" }} onClick={handleClick}>
+      {isloading ? (
+        <Loader active size="huge" inline="centered" />
+      ) : (
+        <img src={catImageUrl} width={500} height="auto" />
+      )}
+
+      <button style={{ marginTop: 18 }} onClick={handleClick}>
         DayCat
       </button>
     </div>
   );
 };
 
+export const getServerSideProps: GetServerSideProps<
+  IndexPageProps
+> = async () => {
+  const catImage = await fetchCatImage();
+  return {
+    props: {
+      initialCatimageurl: catImage.url,
+    },
+  };
+};
 export default Home;
